385afa3bd9ffc63fd4c1fc45fae27906f5b27725
Diogo <dutradda@gmail.com>
2018-08-09 12:23:59 -0300

    adding material-ui (WIP)

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-relay'
+import { withTheme } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 import withData from '../lib/withData'
 import MediaPreview from '../components/MediaPreview'
+import theme from '../theme'
+
+let AppGrid = props => {
+
+  return (
+    <div>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Paper>
+            <MediaPreview media={props.getMedia} />
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>
+            <Button>
+              Button 1
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>
+            <Button>
+              Button 2
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Button color="secondary">
+        Button 3
+      </Button>
+    </div>
+  )
+}
+// AppGrid = withTheme(theme)(AppGrid)
+
 
 class Index extends Component {
   static displayName = `Index`
 
-  render (props) {
-    return (
-      <div>
-        <MediaPreview media={this.props.getMedia} />
-      </div>
-    )
+  render () {
+    return <AppGrid {...this.props} />
   }
 }
+
+// Index = withTheme(theme)(Index)
 
 export default withData(Index, {
   query: graphql`
---
+++ theme/index.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export default createMuiTheme({
+  palette: {
+    // type: 'dark',
+    primary: {
+      main: '#616161',
+      light: '#8e8e8e',
+      dark: '#373737',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#bf360c',
+      light: '#f9683a',
+      dark: '#870000',
+      contrastText: '#fff'
+    }
+  }
+})
